ca4a8d901dd6b13b749afc08d8f76eada9b42553
bnu <bnufactory@gmail.com>
2012-06-11 11:32:42 +0900

    위젯 탐색 코드 수정.

--- topicflux.controller.php
+++ topicflux.controller.php
@@ -11,12 +11,12 @@ class topicfluxController extends topicflux
 	function triggerDisplayBefore(&$output)
 	{
 		if(Context::get('module') == 'admin') return;
+		if(Context::get('act') == 'dispPageAdminContentModify') return;
 		$current_module_info = Context::get('current_module_info');
 		if($current_module_info->module_type != 'view') return;
-		if(Context::get('act') == 'dispPageAdminContentModify') return;
 
 		debugPrint('TOPICFLUX TRIGGER');
-		if(strpos($output, 'widget="topiccast"') || strpos($output, 'class="topiccast-container"'))
+		if(strpos($output, 'widget="topiccast"') != FALSE || strpos($output, 'class="topiccast-container') != FALSE)
 		{
 			debugPrint('발견');
 			Context::addJsFile('./modules/topicflux/tpl/js/topicflux.common.js');
