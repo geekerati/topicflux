e1109815b5d2c75740d51d75ced0bba235080649
bnu <bnufactory@gmail.com>
2012-08-09 02:20:51 +0900

    스크립트를 jQuery 플러그인 형태로 변경 중.

--- tpl/js/topicflux.common.js
+++ tpl/js/topicflux.common.js
@@ -1,47 +1,46 @@
 (function($) {
 	function debugPrint(msg){if(typeof console == 'object' && typeof console.log == 'function'){console.log(msg)}};
-	$.fn.topicflux = function() {
 
+	var $toolbarContainer = $('<div class="topiccast-toolbar-container"><button>EDIT</button></div>');
+
+	$.topicflux = function() {
+		$('.topiccast-controll-box').addClass('enable');
+		$('.topiccast-container').each(function () {
+			init(this);
+		});
 	};
+
+	$.extend($.topicflux, {
+	});
+
+	$.fn.topicflux = function() {
+	};
+
+	function init(el)
+	{
+		var $_el = $(el);
+		var $_toolbar = $toolbarContainer.clone();
+		var _cast_id = $_el.data('cast-id');
+
+		$_toolbar
+			.on('click', 'button', function() {
+				var _url = window.request_uri.setQuery('act', 'dispTopicfluxEditor').setQuery('cast_id', _cast_id);
+				var _id = 'topicflux-castedit-' + _cast_id;
+				window.open(_url, _id, 'width=900, height=600, location=0, status=0, scrollbars=1');
+			})
+			.addClass('editable');
+
+		$_el.before($_toolbar.width($_el.width() - 2));
+	}
 
 	$(function() {
 		var $pageControllBox = $('.topiccast-controll-box');
 
 		if($pageControllBox.size())
 		{
-			var $widgetContainer = $('.topiccast-container');
-			var $toolbarContainer = $('<div class="topiccast-toolbar-container"><button>EDIT</button></div>');
-			var $btnEdit = $('.edit-enable', $pageControllBox);
-
-			$btnEdit.on('click', function() {
-				$pageControllBox.triggerHandler('edit-enable');
+			$('.edit-enable', $pageControllBox).on('click', function() {
+				$.topicflux();
 				return false;
-			});
-
-			/* !편집 툴바 */
-			$pageControllBox.on('edit-enable', function() {
-				$pageControllBox.addClass('enable');
-				$widgetContainer.each(function() {
-					var $_this = $(this);
-					var $_toolbar = $toolbarContainer.clone();
-					var _cast_id = $_this.data('cast-id');
-
-					$_toolbar.on('click', 'button', function() {
-						var _url = window.request_uri.setQuery('act', 'dispTopicfluxEditor').setQuery('cast_id', _cast_id);
-						window.open(_url, 'id', 'width=900, height=600, location=0, status=0, scrollbars=1');
-					});
-
-					$_this.before($_toolbar.width($_this.width() - 2));
-				});
-				$widgetContainer.triggerHandler('edit-enable');
-			});
-
-			/* !위젯 컨테이너 */
-			$widgetContainer.on('edit-enable', function() {
-				$widgetContainer.addClass('editable');
-			});
-			$widgetContainer.on('edit-disable', function() {
-				var $_this = $(this);
 			});
 		}
 	});
